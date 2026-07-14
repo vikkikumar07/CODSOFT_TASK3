b7ec26737dfbc3c4ea765a145acbfb2e0d544ba1
vikkikumar07 <vikkikumar2201@gmail.com>
2026-07-14 20:07:15 +0530

    add amount deposit function.

--- ATMInterface.java
+++ ATMInterface.java
@@ -1,5 +1,22 @@
 package CODSOFT_TASK3;
+class BankAccount {
+    private double balance;
 
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    // Amount deposit function
+    public void deposit(double amount) {
+        if(amount > 0) {
+            balance += amount;
+            System.out.println(amount +" Deposited Successfuly.");
+        } else {
+            System.out.println("Invalide Amount!");
+        }
+    }
+
+}
 public class ATMInterface {
     
 }
