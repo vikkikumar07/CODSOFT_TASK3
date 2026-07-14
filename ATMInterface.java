d9071fb50bff900c95c99247dcd56758b13e43dc
vikkikumar07 <vikkikumar2201@gmail.com>
2026-07-14 20:14:36 +0530

    add withdraw function in bankAccount

--- ATMInterface.java
+++ ATMInterface.java
@@ -16,6 +16,17 @@ class BankAccount {
         }
     }
 
+    // withdraw amount function
+    public void withdraw(double amount) {
+        if(amount <= 0) {
+            System.out.println("Invalid Amount!");
+        } else if(amount > balance) {
+            System.out.println("Insuffictient Balance!");
+        } else {
+            balance -= amount;
+            System.out.println(amount +" Withdrawn Successfuly.");
+        }
+    }
 }
 public class ATMInterface {
     
